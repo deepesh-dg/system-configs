db25dd313046dae0ca39b9a1aa03dc293f3c4d1c
Deepesh Gupta <34032722+deepesh-dg@users.noreply.github.com>
2024-10-20 17:05:13 +0530

    Update install.sh

--- macos/install.sh
+++ macos/install.sh
@@ -22,15 +22,19 @@ echo "installing mas apps from appstore"
 echo "MacOS System settings configuration started..."
 /bin/bash ~/system-configs/macos/settings.sh
 
+# Set zsh as default shell
+chsh -s $(which zsh)
+
 echo "Installing Oh My Zsh"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "applying new zshrc config"
+source ~/.zshrc
+
 echo "Installing oh-my-zsh plugins"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-completions.git $ZSH_CUSTOM/plugins/zsh-completions
-
-# Set zsh as default shell
-chsh -s $(which zsh)
 
 echo "Setup completed. Restarting your system..."
 
