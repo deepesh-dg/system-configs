c030ff08cca7cb8f471804bec86e450f2f9efa1c
Deepesh Gupta <34032722+deepesh-dg@users.noreply.github.com>
2024-10-20 15:09:12 +0530

    Update install.sh

--- macos/install.sh
+++ macos/install.sh
@@ -3,13 +3,6 @@
 echo "clone git system configs"
 git clone https://github.com/deepesh-dg/system-configs.git ~/system-configs
 
-echo "Installing Oh My Zsh"
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-echo "Installing oh-my-zsh plugins"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-completions.git $ZSH_CUSTOM/plugins/zsh-completions
-
 echo "Installing dotfiles"
 
 # Symlink dotfiles to home directory
@@ -28,6 +21,13 @@ echo "installing mas apps from appstore"
 
 echo "MacOS System settings configuration started..."
 /bin/bash ~/system-configs/macos/settings.sh
+
+echo "Installing Oh My Zsh"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "Installing oh-my-zsh plugins"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-completions.git $ZSH_CUSTOM/plugins/zsh-completions
 
 # Set zsh as default shell
 chsh -s $(which zsh)
