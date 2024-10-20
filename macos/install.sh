1c6544da2375680d723ee614954ee3563c2a8222
Deepesh Gupta <34032722+deepesh-dg@users.noreply.github.com>
2024-10-20 17:14:47 +0530

    Update install.sh

--- macos/install.sh
+++ macos/install.sh
@@ -36,6 +36,4 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/p
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-completions.git $ZSH_CUSTOM/plugins/zsh-completions
 
-echo "Setup completed. Restarting your system..."
-
-sudo shutdown -r now
+echo "Setup completed. Please restart your system..."
